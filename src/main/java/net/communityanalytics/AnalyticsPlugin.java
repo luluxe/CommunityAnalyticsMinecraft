a911e43ba8a1f34401f57aeac838a8d96192349a
Maxlego08 <maxlego08@sfr.fr>
2023-01-26 11:45:58 +0100

    :tada: First commit

---
+++ src/main/java/net/communityanalytics/AnalyticsPlugin.java
@@ -0,0 +1,21 @@
+package net.communityanalytics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class AnalyticsPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("Chargement du plugin !");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("Je me décharge !");
+	}
+
+}
